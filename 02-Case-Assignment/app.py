649acbef77593ec9db125afbde277a5ffe5a3f40
Fraol Abebe <fraolabebe@Fraols-Air.fios-router.home>
2021-01-06 00:02:27 -0500

    changed my work

--- 02-Case-Assignment/app.py
+++ 02-Case-Assignment/app.py
@@ -1,40 +1,25 @@
-from flask import Flask, redirect, render_tamplate
+from flask import Flask, redirect, render_template
 from flask_pymongo import  PyMongo
 import pandas as pd
-import pymongo
-from splinter import Browser 
 from webdriver_manager.chrome import ChromeDriverManager
-from bs4 import BeautifulSoup
+from scrape_mars import scrape_all
 
-
-#instatiate Flask App
 app = Flask(__name__)
-client = PyMongo(app, uri='mongodb://localhost:27017/mars_db')
 
+conn = 'mongodb://localhost:27017/mars_db'
+mongo = PyMongo(app,conn)
 
 @app.route('/')
 def index():
-    return(render_tamplate)
-        # some code here
+    mars_data = mongo.db.mars.find_one()
+    return(render_template('index.html', mars_data=mars_data))
+
 @app.route('/scrape')
 def scrape():
-    
+    data_document = scrape_all()
+    # mars.insert_one(data_document)
+    mongo.db.mars.update_one({}, {'$set': data_document}, upsert=True)
+    return redirect('/')
 
-    # Connect to mars_app database
-    db = client.mars_app
-
-    # Connect to mars collection
-    mars = db.mars
-
-    # Gather document to insert
-    data_document = scrape_mars.scrape_all()
-
-    #mars.insert_one(data_document)
-
-
-    mars.update_one({}, {'$set': data_document}, upsert=True
-    
-
-
-if __name__ == '__main__':
+if __name__=='__main__':
     app.run(debug=True)
